582bf11635211e81c25654a58d6d1b6677df5874
John <johnjohnjohnkimchi@gmail.com>
2026-03-21 18:10:55 -0400

    Completed Phase 2 of RBCheck

---
+++ main.py
@@ -0,0 +1,76 @@
+import sqlite3
+import os
+import pprint as p
+import datetime as d
+import re
+
+def parseTransaction(text):
+    '''
+    Cheeky function finding where the embedded message within attributedBody starts and ends.
+
+    @returns string text of the transaction
+    '''
+    startPos = text.find("RBC: ")
+    if startPos == -1:
+        startPos = text.find("DEPOSIT")
+    if startPos == -1:
+        startPos = text.find("AVAIL CREDIT")
+        return "bw"
+    endPos = text.find("HELP-TXT HELP")
+
+    return text[startPos:endPos]
+
+def transactionAnalysis(text):
+    transactionType = ""
+    if text == "bw":
+        transactionType = "Balance Warning!"
+    amount = re.search('\\$[\\d.,]*\\d', text)
+    amount = amount.group() if amount else "Null"
+
+    place = re.search(r'AT (.+?)(?:\. STOP-TXT| STOP-TXT)', text)
+    place = place.group(1) if place else ""
+
+    if "DEPOSIT" in text:
+        transactionType = "Deposit"
+    elif "WITHDRAWAL" in text:
+        transactionType = "Withdrawal"
+    elif "PURCHASE" in text:
+        transactionType = "CC Purchase"
+    elif "PAYMENT OF" in text:
+        transactionType = "Credit Card Payment"
+    return amount, place, transactionType
+
+
+transactions = [] # List taking in a dictionary of Date, Transaction Source and Amount
+
+database = os.path.expanduser("~/Library/Messages/chat.db")
+
+conn = sqlite3.connect(database)
+cursor = conn.cursor()
+
+query = "SELECT datetime(m.date / 1000000000 + 978307200, 'unixepoch', 'localtime'), m.attributedBody " \
+"FROM message as m, handle as h " \
+"WHERE h.id=72272 AND h.ROWID=m.handle_id " \
+"ORDER BY m.date ASC;" \
+
+cursor.execute(query)
+
+result = cursor.fetchall()
+
+
+for query in result:
+    transaction = {}
+    
+    transaction["Time"] = query[0]
+    analysis = transactionAnalysis(parseTransaction(str(query[1]).upper()))
+
+    transaction["Amount"] = analysis[0]
+    transaction["Place"] = analysis[1] 
+    transaction["Type"] = analysis[2]
+
+    transactions.append(transaction)
+
+
+with open("output.txt", "w") as f:
+    for i in range(len(transactions)):
+        print(transactions[i], file = f)
